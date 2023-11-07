2314d4d7635346c25dded7844aa791d114ada904
samko <smlprinc@gmail.com>
2023-11-07 08:59:14 +0100

    AHOJKY

---
+++ input.py
@@ -0,0 +1,11 @@
+
+print("jmeno ?")
+user = input()
+if user == "jindra":
+    print("kys")
+elif user == "kojima":
+    print("panbuh zehnej " + user)
+elif user == "adam":
+    print(user+"e bez do pici")
+else:
+    print("cusbus")
